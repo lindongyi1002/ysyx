102f31bf4e19215955217c2982689251f292eeb1
ysyx_23051555-lindongyi <1292471097@qq.com>
2023-05-22 17:17:36 +0800

    learn to ex7

---
+++ entry/c_learn/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 23;
+    int height = 183;
+
+    printf("I am %d years old.\n",age);
+    printf("I am %dcm tall.\n",height);
+
+    return 0;
+}
---
+++ entry/c_learn/ex4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 23;
+    int height;
+
+    printf("I am %d years old.\n");
+    printf("I am %dcm tall.\n",height);
+
+    return 0;
+}
